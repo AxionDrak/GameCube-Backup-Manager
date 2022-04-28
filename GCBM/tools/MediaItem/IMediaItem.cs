a968782baa85808055346466bb5fede0f4510425
Laete Meireles <laetemn@gmail.com>
2022-04-27 21:22:40 -0300

    Create IMediaItem.cs

---
+++ GCBM/tools/MediaItem/IMediaItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PluginBurnMedia.Interop;
+
+namespace PluginBurnMedia.MediaItem
+{
+    interface IMediaItem
+    {
+        /// <summary>
+        /// Returns the full path of the file or directory
+        /// </summary>
+        string Path { get; }
+
+        /// <summary>
+        /// Returns the size of the file or directory to the next largest sector
+        /// </summary>
+        Int64 SizeOnDisc { get; }
+
+        /// <summary>
+        /// Returns the Icon of the file or directory
+        /// </summary>
+        System.Drawing.Image FileIconImage { get; }
+
+        // Adds the file or directory to the directory item, usually the root.
+        bool AddToFileSystem(IFsiDirectoryItem rootItem);
+    }
+}
