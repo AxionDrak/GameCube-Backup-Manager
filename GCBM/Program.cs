ad6cbf9de4c9172609040b88233cb816371821e8
Laete Meireles <laetemn@gmail.com>
2022-04-27 21:29:26 -0300

    Create Program.cs

---
+++ GCBM/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GCBM
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            IniFile configIniFile = new IniFile("config.ini");
+
+            //Pega o nome do processo deste programa
+            string nomeProcesso = Process.GetCurrentProcess().ProcessName;
+            //Busca os processos com este nome que estão em execução
+            Process[] processos = Process.GetProcessesByName(nomeProcesso);
+
+            if (File.Exists("config.ini"))
+            {
+                if (configIniFile.IniReadBool("SEVERAL", "MultipleInstances") == false)
+                {
+                    //Pega o nome do processo deste programa
+                    //string nomeProcesso = Process.GetCurrentProcess().ProcessName;
+                    //Busca os processos com este nome que estão em execução
+                    //Process[] processos = Process.GetProcessesByName(nomeProcesso);
+                    //Se já houver um aberto
+                    if (processos.Length > 1)
+                    {
+                        MessageBox.Show(GCBM.Properties.Resources.CannotOpenTwoInstances, "GameCube Backup Manager", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        Application.EnableVisualStyles();
+                        Application.SetCompatibleTextRenderingDefault(false);
+                        Application.Run(new frmMain());
+                    }
+                }
+                else
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new frmMain());
+                }
+            }
+            else
+            {
+                if (processos.Length > 1)
+                {
+                    MessageBox.Show(GCBM.Properties.Resources.CannotOpenTwoInstances, "GameCube Backup Manager", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Application.Exit();
+                }
+                else
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new frmMain());
+                }
+            }
+        }
+    }
+}
