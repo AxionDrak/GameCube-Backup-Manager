38845399340c24e8d62805561e1589934277a598
Laete Meireles <laetemn@gmail.com>
2022-04-27 21:29:30 -0300

    Create AssemblyInfo.cs

---
+++ GCBM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("GameCube Backup Manager")]
+[assembly: AssemblyDescription("Nintendo GameCube ISO and GCM file manager software.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Axion Drak ©  2022")]
+[assembly: AssemblyProduct("GameCube Backup Manager")]
+[assembly: AssemblyCopyright("Axion Drak ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fc3f3e82-283b-4ca0-b076-9b2737cb2773")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
