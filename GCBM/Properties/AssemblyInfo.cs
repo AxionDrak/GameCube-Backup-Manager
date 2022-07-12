28d8f0137c887aca828253dbb3959d891ff19a7e
Laete Meireles <laetemn@gmail.com>
2022-07-12 12:38:29 -0300

    Update AssemblyInfo.cs

--- GCBM/Properties/AssemblyInfo.cs
+++ GCBM/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
