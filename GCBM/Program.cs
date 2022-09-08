bac6d700e4f0602e94d2ed31656d2c1ca39bea8e
sjohnson1021 <sjohnson1021@outlook.com>
2022-09-08 05:51:36 -0500

    Good morning.. ? Languages working, changelog updated, ensure config is properly written upon first launch.

--- GCBM/Program.cs
+++ GCBM/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -13,7 +14,19 @@ internal static class Program
 {
     public static Form SplashScreen;
     private static Form MainForm;
+    private static readonly string GET_CURRENT_PATH = Directory.GetCurrentDirectory();
+    
+    private static readonly string GAMES_DIR = "games";
+    private static readonly string TEMP_DIR = System.IO.Path.DirectorySeparatorChar + "temp";
 
+    private static readonly string COVERS_DIR =
+        System.IO.Path.DirectorySeparatorChar + "covers" + System.IO.Path.DirectorySeparatorChar + "cache";
+
+    private static readonly string MEDIA_DIR =
+        System.IO.Path.DirectorySeparatorChar + "media" + System.IO.Path.DirectorySeparatorChar + "covers";
+
+    private const string INI_FILE = "config.ini";
+    private static readonly string PROG_UPDATE = "10/07/2022";
     public enum Language
     {
         Portuguese, English, Spanish, Korean, French, German, Japanese
@@ -70,6 +83,7 @@ internal static class Program
         }
         else
         {
+            DefaultConfigSave();
             if (processos.Length > 1)
             {
                 _ = MessageBox.Show(Resources.CannotOpenTwoInstances, "GameCube Backup Manager", MessageBoxButtons.OK,
@@ -82,6 +96,103 @@ internal static class Program
             }
         }
     }
+
+    public static void DefaultConfigSave()
+    {
+        if (System.IO.File.Exists(GET_CURRENT_PATH + System.IO.Path.DirectorySeparatorChar + INI_FILE))
+        {
+            var CONFIG_INI_FILE = new IniFile("config.ini");
+            // GCBM
+            CONFIG_INI_FILE.IniWriteString("GCBM", "ProgUpdated", PROG_UPDATE);
+            //CONFIG_INI_FILE.IniWriteString("GCBM", "ProgVersion", VERSION());
+            CONFIG_INI_FILE.IniWriteString("GCBM", "ConfigUpdated", DateTime.Now.ToString("dd/MM/yyyy"));
+            CONFIG_INI_FILE.IniWriteString("GCBM", "Language", Resources.GCBM_Language);
+            CONFIG_INI_FILE.IniWriteString("GCBM", "TranslatedBy", Resources.GCBM_TranslatedBy);
+            // General
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "DiscClean", true);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "DiscDelete", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "ExtractZip", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "Extract7z", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "ExtractRar", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "ExtractBZip2", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "ExtractSplitFile", false);
+            CONFIG_INI_FILE.IniWriteBool("GENERAL", "ExtractNwb", false);
+            CONFIG_INI_FILE.IniWriteInt("GENERAL", "FileSize", 0);
+            CONFIG_INI_FILE.IniWriteString("GENERAL", "TemporaryFolder", GET_CURRENT_PATH + TEMP_DIR);
+            // Several
+            CONFIG_INI_FILE.IniWriteInt("SEVERAL", "AppointmentStyle", 0);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "CheckMD5", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "CheckSHA1", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "CheckNotify", true);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "NetVerify", true);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "RecursiveMode", true);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "TemporaryBuffer", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "WindowMaximized", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "DisableSplash", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "Screensaver", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "LoadDatabase", true);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "MultipleInstances", false);
+            CONFIG_INI_FILE.IniWriteBool("SEVERAL", "LaunchedOnce", true);
+            // TransferSystem
+            CONFIG_INI_FILE.IniWriteBool("TRANSFERSYSTEM", "FST", false);
+            CONFIG_INI_FILE.IniWriteBool("TRANSFERSYSTEM", "ScrubFlushSD", false);
+            CONFIG_INI_FILE.IniWriteInt("TRANSFERSYSTEM", "ScrubAlign", 0);
+            CONFIG_INI_FILE.IniWriteString("TRANSFERSYSTEM", "ScrubFormat", "DiscEx");
+            CONFIG_INI_FILE.IniWriteInt("TRANSFERSYSTEM", "ScrubFormatIndex", 1);
+            CONFIG_INI_FILE.IniWriteBool("TRANSFERSYSTEM", "Wipe", false);
+            CONFIG_INI_FILE.IniWriteBool("TRANSFERSYSTEM", "XCopy", true);
+            // Covers
+            CONFIG_INI_FILE.IniWriteBool("COVERS", "DeleteCovers", false);
+            CONFIG_INI_FILE.IniWriteBool("COVERS", "CoverRecursiveSearch", false);
+            CONFIG_INI_FILE.IniWriteBool("COVERS", "TransferCovers", false);
+            CONFIG_INI_FILE.IniWriteBool("COVERS", "WiiFlowCoverUSBLoader", false);
+            CONFIG_INI_FILE.IniWriteBool("COVERS", "GXCoverUSBLoader", true);
+            CONFIG_INI_FILE.IniWriteString("COVERS", "CoverDirectoryCache", GET_CURRENT_PATH + COVERS_DIR);
+            CONFIG_INI_FILE.IniWriteString("COVERS", "WiiFlowCoverDirectoryDisc", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "WiiFlowCoverDirectory2D", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "WiiFlowCoverDirectory3D", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "WiiFlowCoverDirectoryFull", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "GXCoverDirectoryDisc", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "GXCoverDirectory2D", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "GXCoverDirectory3D", "");
+            CONFIG_INI_FILE.IniWriteString("COVERS", "GXCoverDirectoryFull", "");
+            // Titles
+            CONFIG_INI_FILE.IniWriteBool("TITLES", "GameCustomTitles", false);
+            CONFIG_INI_FILE.IniWriteBool("TITLES", "GameTdbTitles", false);
+            CONFIG_INI_FILE.IniWriteBool("TITLES", "GameInternalName", true);
+            CONFIG_INI_FILE.IniWriteBool("TITLES", "GameXmlName", false);
+            CONFIG_INI_FILE.IniWriteString("TITLES", "LocationTitles",
+                "%APP%" + System.IO.Path.DirectorySeparatorChar + "titles.txt");
+            CONFIG_INI_FILE.IniWriteString("TITLES", "LocationCustomTitles",
+                "%APP%" + System.IO.Path.DirectorySeparatorChar + "custom-titles.txt");
+            CONFIG_INI_FILE.IniWriteInt("TITLES", "TitleLanguage", 0);
+            // Dolphin Emulator
+            CONFIG_INI_FILE.IniWriteString("DOLPHIN", "DolphinFolder", "");
+            CONFIG_INI_FILE.IniWriteBool("DOLPHIN", "DolphinDX11", true);
+            CONFIG_INI_FILE.IniWriteBool("DOLPHIN", "DolphinDX12", false);
+            CONFIG_INI_FILE.IniWriteBool("DOLPHIN", "DolphinVKGL", false);
+            CONFIG_INI_FILE.IniWriteBool("DOLPHIN", "DolphinLLE", false);
+            CONFIG_INI_FILE.IniWriteBool("DOLPHIN", "DolphinHLE", true);
+            // Updates
+            CONFIG_INI_FILE.IniWriteBool("UPDATES", "UpdateVerifyStart", false);
+            CONFIG_INI_FILE.IniWriteBool("UPDATES", "UpdateBetaChannel", false);
+            CONFIG_INI_FILE.IniWriteBool("UPDATES", "UpdateFileLog", false);
+            CONFIG_INI_FILE.IniWriteBool("UPDATES", "UpdateServerProxy", false);
+            CONFIG_INI_FILE.IniWriteString("UPDATES", "ServerProxy", "");
+            CONFIG_INI_FILE.IniWriteString("UPDATES", "UserProxy", "");
+            CONFIG_INI_FILE.IniWriteString("UPDATES", "PassProxy", "");
+            CONFIG_INI_FILE.IniWriteInt("UPDATES", "VerificationInterval", 0);
+            // Manager Log
+            CONFIG_INI_FILE.IniWriteInt("MANAGERLOG", "LogLevel", 0);
+            CONFIG_INI_FILE.IniWriteBool("MANAGERLOG", "LogSystemConsole", false);
+            CONFIG_INI_FILE.IniWriteBool("MANAGERLOG", "LogDebugConsole", false);
+            CONFIG_INI_FILE.IniWriteBool("MANAGERLOG", "LogWindow", false);
+            CONFIG_INI_FILE.IniWriteBool("MANAGERLOG", "LogFile", true);
+            // Language
+            CONFIG_INI_FILE.IniWriteInt("LANGUAGE", "ConfigLanguage", 1);
+        }
+    }
+
     #region Detect OS Language
 
     /// <summary>
